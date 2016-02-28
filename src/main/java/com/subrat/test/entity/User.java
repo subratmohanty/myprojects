b474137101e0db1f7258a78eea09c6d242529d2a
subratmohanty <subratmohanty23@gmail.com>
2016-02-28 16:36:28 +0530

    First Commit

---
+++ src/main/java/com/subrat/test/entity/User.java
@@ -0,0 +1,22 @@
+package com.subrat.test.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Integer userId;
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	
+}
